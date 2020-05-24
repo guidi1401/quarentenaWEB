d9d3ce8b2328da4e79de287f41f17e103fb86611
guidi1401 <amandaguidi2003@gmail.com>
2020-05-23 21:38:45 -0300

    exercicio1.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> Exercícios Quarentena </title>
+    </head>
+    <body>
+        <h1>Exercícios Quarentena</h1>
+        <p><a href="exercicio1.php"> Exercício 1 - Quarentena </a> </p>
+    <body>
+</html>
